69d4c1254bc5dd84e639980027e4c925e66bbacd
Renzo Beltran <renzobeltran@Renzos-MacBook-Pro.local>
2021-10-30 12:14:29 -0500

    Add: data_list file with all the products in the store
    This file contains all the products
    availables to this challenge with their
    default prices

--- data/data_list.rb
+++ data/data_list.rb
@@ -1,1 +1,15 @@
-# Data file for the project
+require './classes/Product.rb'
+
+module Variables
+  productA = Product.new("A", 2.00)
+  productB = Product.new("B", 12.00)
+  productC = Product.new("C", 1.25)
+  productD = Product.new("D", 0.15)
+  
+  PRODUCTS_LIST = { 
+    A: productA,
+    B: productB, 
+    C: productC, 
+    D: productD
+  }
+end
